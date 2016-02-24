bc92a48795e5efec169d190d3564171fc3494684
Luciano <lucianobruzzoni@yahoo.com>
2016-02-24 01:48:55 +0000

    Times now displayed on human readable time arguments

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+app.use(express.static('client'));
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'jade');
 
-app.use(express.static(__dirname, 'client'));
+var months = ["January", "February", "March", "April", "May", "June", 
+              "July", "August", "September", "October", "November", "December"];
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+app.get('/:date', function(req, res) {
+  var unix; 
+  var natural = null;
+  var date = req.params.date;
+  date = new Date(date);
+  unix = date.setTime(date.getTime());
+  
+  if(unix){
+    var month = months[date.getMonth()];
+    natural = month + " " + date.getDate() + ", " + date.getFullYear();
+  }
+  
+  res.send({
+    "unix" : unix,
+    "natural" : natural
+  });
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = app.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("Chat server listening at port 3000");
 });
